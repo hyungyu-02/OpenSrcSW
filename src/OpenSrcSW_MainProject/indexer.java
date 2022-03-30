600cb18f13fcc7269ea1976c8297c472a7dbf5f7
hyungyu0209 <yona20020209@gmail.com>
2022-03-30 20:38:20 +0900

    ctrl + shift + f

--- src/OpenSrcSW_MainProject/indexer.java
+++ src/OpenSrcSW_MainProject/indexer.java
@@ -33,101 +33,93 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class indexer {
-	
+
 	private String input_file_path;
 	private String output_file = "./index.post";
-	
+
 	public indexer(String file) {
 		this.input_file_path = file;
 	}
-	
-	
-	public void convertPost() throws ParserConfigurationException, SAXException, IOException, TransformerException, ClassNotFoundException {
-		
+
+	public void convertPost() throws ParserConfigurationException, SAXException, IOException, TransformerException,
+			ClassNotFoundException {
+
 		File file = new File(input_file_path);
-		
+
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		Document document = db.parse(file);
 		document.getDocumentElement().normalize();
 		NodeList nList = document.getElementsByTagName("doc");
-		
-		
+
 		File idxPostFile = new File("./index.post");
-		
-		
+
 		HashMap<String, Integer> indexerMap = new HashMap<>();
-		HashMap<String,String> resultMap = new HashMap<String,String>();
-		
-		
+		HashMap<String, String> resultMap = new HashMap<String, String>();
+
 		String splitBySharp[];
 		String splitByColon[];
-		
-		for(int i = 0; i < nList.getLength(); i++) {
+
+		for (int i = 0; i < nList.getLength(); i++) {
 			Node nNode = nList.item(i);
-			
+
 			String idxerBodyData;
-			
-			if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 				Element eElement = (Element) nNode;
-				
 				idxerBodyData = eElement.getElementsByTagName("body").item(0).getTextContent();
-				
+
 				splitBySharp = idxerBodyData.split("#");
-				
-				for(int j = 0; j < splitBySharp.length; j++) {
+
+				for (int j = 0; j < splitBySharp.length; j++) {
 					splitByColon = splitBySharp[j].split(":");
 					if (splitByColon.length == 2) {
-						//int rep = Integer.parseInt(splitByColon[1]);
-
 						if (indexerMap.get(splitByColon[0]) == null) {
 							indexerMap.put(splitByColon[0], 1);
-						}
-						else {
-							indexerMap.put(splitByColon[0], indexerMap.get(splitByColon[0])+1);
+						} else {
+							indexerMap.put(splitByColon[0], indexerMap.get(splitByColon[0]) + 1);
 						}
 					}
 				}
-				
+
 			}
 		}
-		
-		
-		
-		for(int i = 0; i < nList.getLength(); i++) {
-			HashMap<String, Double> TFIDFMap = new HashMap<String,Double>();
-			
+
+		for (int i = 0; i < nList.getLength(); i++) {
+			HashMap<String, Double> TFIDFMap = new HashMap<String, Double>();
+
 			Node nNode = nList.item(i);
-			
+
 			String idxerBodyData;
-			
-			if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 				Element eElement = (Element) nNode;
-				
+
 				idxerBodyData = eElement.getElementsByTagName("body").item(0).getTextContent();
-				
+
 				splitBySharp = idxerBodyData.split("#");
-				
-				for(int j = 0; j < splitBySharp.length; j++) {
+
+				for (int j = 0; j < splitBySharp.length; j++) {
 					splitByColon = splitBySharp[j].split(":");
 					if (splitByColon.length == 2) {
 						int rep = Integer.parseInt(splitByColon[1]);
-						
-						if(indexerMap.get(splitByColon[0]) != null) {
+
+						if (indexerMap.get(splitByColon[0]) != null) {
 							double weight;
-							//weight = Math.round(((double)((rep) * Math.log(nList.getLength()/TFIDFMap.get(splitByColon[0])))) * 100)/100.0;
+							// weight = Math.round(((double)((rep) *
+							// Math.log(nList.getLength()/TFIDFMap.get(splitByColon[0])))) * 100)/100.0;
 							weight = (double) ((rep) * Math.log(nList.getLength() / indexerMap.get(splitByColon[0])));
 							weight = Math.round(weight * 100) / 100.0;
-							
+
 							TFIDFMap.put(splitByColon[0], weight);
-							
-							if(resultMap.get(splitByColon[0]) == null) {
+
+							if (resultMap.get(splitByColon[0]) == null) {
 								String resultStr = "";
 								resultStr += Integer.toString(i) + " " + Double.toString(weight);
 								resultMap.put(splitByColon[0], resultStr);
-							}
-							else {
-								String resultStr = resultMap.get(splitByColon[0]) + " " + Integer.toString(i) + " " + Double.toString(weight);
+							} else {
+								String resultStr = resultMap.get(splitByColon[0]) + " " + Integer.toString(i) + " "
+										+ Double.toString(weight);
 								resultMap.put(splitByColon[0], resultStr);
 							}
 						}
@@ -135,32 +127,31 @@ public class indexer {
 				}
 			}
 			Iterator<String> it = indexerMap.keySet().iterator();
-			while(it.hasNext()) {
+			while (it.hasNext()) {
 				String keys = it.next();
-				if(TFIDFMap.get(keys) == null) {
-					if(resultMap.get(keys) == null) {
+				if (TFIDFMap.get(keys) == null) {
+					if (resultMap.get(keys) == null) {
 						resultMap.put(keys, Integer.toString(i) + " 0.00");
-					}
-					else {
+					} else {
 						String str = resultMap.get(keys) + " " + Integer.toString(i) + " 0.00";
 						resultMap.put(keys, str);
 					}
 				}
 			}
 		}
-		
+
 		FileOutputStream fileStream = new FileOutputStream("./index.post");
 		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileStream);
-		
+
 		objectOutputStream.writeObject(resultMap);
 		objectOutputStream.close();
-		
+
 		FileInputStream fstream = new FileInputStream("./index.post");
 		ObjectInputStream objectInputstream = new ObjectInputStream(fstream);
-		
+
 		Object obj = objectInputstream.readObject();
 		objectInputstream.close();
-		
+
 		HashMap<String, String> hashMap = (HashMap) obj;
 		Iterator<String> it = resultMap.keySet().iterator();
 		while (it.hasNext()) {
@@ -168,20 +159,8 @@ public class indexer {
 			String value = resultMap.get(key);
 			System.out.println(key + " : " + value);
 		}
-		
-		
+
 		System.out.println("4주차 실행완료");
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
