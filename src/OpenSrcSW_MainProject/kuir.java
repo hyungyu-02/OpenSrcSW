fe5ee520208021cebc9ef2af2b2d28dc5108a6bc
hyungyu0209 <yona20020209@gmail.com>
2022-04-06 17:45:10 +0900

    new branch feature and make searcher success

--- src/OpenSrcSW_MainProject/kuir.java
+++ src/OpenSrcSW_MainProject/kuir.java
@@ -9,23 +9,32 @@ import org.xml.sax.SAXException;
 
 public class kuir {
 
-	public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, SAXException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-		
-		String command = args[0];
-		String path = args[1];
-		
-		if(command.equals("-c")){
-			makeCollection collection = new makeCollection(path);
-			collection.makeXml();
-		}
-		else if(command.equals("-k")) {
-			makeKeyword keyword = new makeKeyword(path);
-			keyword.convertXml();
-		}
-		else if(command.equals("-i")) {
-			indexer index = new indexer(path);
-			index.convertPost();
-		}
-	}
+   public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, SAXException, ClassNotFoundException {
+      // TODO Auto-generated method stub
+      
+      String command = args[0];
+      String path = args[1];
+      String command2 = args[2];
+      String query = args[3];
+      
+      if(command.equals("-c")){
+         makeCollection collection = new makeCollection(path);
+         collection.makeXml();
+      }
+      else if(command.equals("-k")) {
+         makeKeyword keyword = new makeKeyword(path);
+         keyword.convertXml();
+      }
+      else if(command.equals("-i")) {
+         indexer index = new indexer(path);
+         index.convertPost();
+      }
+      else if(command.equals("-s")) {
+         System.out.print("Query : ");
+         if (command2.equals("-q")) {
+            searcher search = new searcher(path, query);
+            search.query();
+         }
+      }
+   }
 }
